d5fc2ff1341aa3943edce7e2bcf3fc9a732f0684
codex-yasxh <adit7ya0patanwar@outlook.com>
2025-11-07 19:28:44 +0530

    section6

---
+++ section6/bookStoreAPI/db/books.js
@@ -0,0 +1,20 @@
+
+
+
+exports.BOOKS = [
+    {   
+        id : 1,
+        name : "book one",
+        author : "one"
+    },
+    {   
+        id :2,
+        name : "book two",
+        author : "two"
+    },
+    {   
+        id :3,
+        name : "book three",
+        author : "three"
+    },
+]
