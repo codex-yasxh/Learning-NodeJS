73935d9bb27323236af02f573033bc661c293c22
codex-yasxh <adit7ya0patanwar@outlook.com>
2025-11-08 16:02:32 +0530

    section 7

---
+++ section7/node-ORMs/db/index.js
@@ -0,0 +1,9 @@
+
+
+const  { drizzle } =  require('drizzle-orm/node-postgres');
+
+
+// postgres: //<username> :< password>@<host> :< port>/<db_name>
+const db = drizzle(process.env.DATABASE_URL); //database instance
+
+module.exports = db;
